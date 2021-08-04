f59f811290cb7fd2f5aad0a42395d64a641a0ee7
Neil Kuan <guan840912@gmail.com>
2021-08-04 19:42:46 +0800

    first commit

--- ppp/ppp_stack.py
+++ ppp/ppp_stack.py
@@ -1,15 +1,15 @@
-from aws_cdk import core as cdk
-
-# For consistency with other languages, `cdk` is the preferred import name for
-# the CDK's core module.  The following line also imports it as `core` for use
-# with examples from the CDK Developer's Guide, which are in the process of
-# being updated to use `cdk`.  You may delete this import if you don't need it.
-from aws_cdk import core
+from aws_cdk import (
+    core as cdk,
+    aws_s3 as s3,
+)
 
 
 class PppStack(cdk.Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.Construct, construct_id: str, bucketname: str or None = None,**kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
+        # if not defeined, use default bucket name 'defaultbucketname'.
+        s3.Bucket(self, 'bucket', bucket_name=bucketname or 'defaultbucketname')
+    # stack methods.
+    def add_bucket(self, name: str or None = None):
+        return s3.Bucket(self, 'bucket'+name, bucket_name=name or 'defaultbucketname')
